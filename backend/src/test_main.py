e6c8a4ebe35e0058414cb4f426b8393e0dcc4fe0
Mikhail Sednev <m.sednev@ajarus.com>
2023-11-02 18:34:26 +0200

    check unittests, mocks e.t.c

--- backend/src/test_main.py
+++ backend/src/test_main.py
@@ -1,7 +1,12 @@
 import pytest
 import pydantic
+import requests
+import json
+from unittest.mock import MagicMock, Mock, patch
 from fastapi.testclient import TestClient
 from typing import List, Dict
+import requests_mock
+import http.client
 
 from config import get_config
 from server import create_application, provide_dependencies_graph
@@ -12,6 +17,99 @@ from routes.clouds import create_clouds_router, create_cloud_router_dependencies
 from schemas.clouds import SearchCloudsResponse, SearchCloudsRequest, CloudRequestFilter, CloudRequestSort
 from fixtures.clouds import clouds, azure_cloud, upcloud_cloud, google_cloud, do_cloud, aws_cloud
 from geo.geo import Point, get_distance_between_two_points
+
+# TODO tests using patch
+
+# def requests_side_effect(*args):
+#     vals = {(1, 2): 1, (2, 3): 2, (3, 4): 3}
+#     return vals[args]
+
+# def test_requests_with_side_effects():
+#     mock = MagicMock(side_effect=requests_side_effect)
+#     assert mock(1,2) == 1
+#     assert mock(2,3) == 2
+#     assert mock(3,4) == 3
+
+
+# @patch("http.client.HTTPResponse")
+# def requests_mock(*args, **kwargs):
+#     # Implement the desired behavior of the mock here
+#     response = http.client.HTTPResponse()
+#     response.status = 200
+#     response.read.return_value = {}
+#     return response
+
+# @patch.object(http.client.HTTPConnection, "request", requests_mock)
+# def test_my_function():
+#     conn = http.client.HTTPConnection("example.com")
+#     conn.request("GET", "/")
+#     response = conn.getresponse()
+#     pass
+
+
+# @patch("http.client.HTTPResponse")
+# def requests_mock(*args, **kwargs):
+#     # Implement the desired behavior of the mock here
+#     response = http.client.HTTPResponse()
+#     response.status = 200
+#     response.read.return_value = {}
+#     return response
+
+# def test_my_function():
+    # with patch.object(http.client.HTTPConnection, "getresponse", requests_mock):
+        # conn = http.client.HTTPConnection("example.com")
+        # conn.request("GET", "/")
+        # response = conn.getresponse()
+    #     pass
+
+# @pytest.fixture
+# def mock_response():
+#     mock_response = MagicMock()
+#     mock_response.status = 200
+#     mock_response.read.return_value = b'{}'
+#     return mock_response
+
+# @pytest.fixture
+# def mock_http_client(mock_response):
+#     with patch.object(http.client.HTTPConnection, 'getresponse', return_value=mock_response):
+#         yield
+
+# @pytest.mark.usefixtures('mock_http_client')
+# def test_my_function():
+#     conn = http.client.HTTPConnection("example.com")
+#     conn.request("GET", "/")
+#     response = conn.getresponse()
+#     assert response.status == 200
+
+@pytest.fixture
+def clouds_body(clouds: List[AivenCloud]) -> AivenClouds:
+    return AivenClouds(
+        clouds=clouds
+    )
+
+
+@pytest.fixture
+def mock_http_client_with_clouds(clouds_body):
+    mock_response = Mock()
+    mock_response.status = 200
+    mock_response.read.return_value = json.dumps(clouds_body.dict()).encode()
+    with patch.object(http.client.HTTPConnection, 'getresponse', return_value=mock_response):
+        yield
+
+@pytest.mark.usefixtures('mock_http_client_with_clouds')
+def test_my_function(clouds_body: AivenClouds):
+    conn = http.client.HTTPConnection("example.com")
+    conn.request("GET", "/")
+    response = conn.getresponse()
+    assert response.status == 200
+
+    response_body = json.loads(response.read().decode())
+    response_body_formatted = AivenClouds(
+        **response_body
+    )
+    assert response_body_formatted == clouds_body
+
+
 
 @pytest.fixture(name="application_dependencies_graph")
 def application_dependencies_graph(clouds: List[AivenCloud]) -> ApplicationDependenciesGraph:
@@ -39,6 +137,7 @@ def application_dependencies_graph(clouds: List[AivenCloud]) -> ApplicationDepen
     #
 
     return test_application_dependencies_graph
+
 
 @pytest.fixture(name="client_with_fixed_clouds")
 def client_with_fixed_clouds(application_dependencies_graph: ApplicationDependenciesGraph) -> TestClient:
